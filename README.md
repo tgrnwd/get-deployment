1a795e2f31e1dddc07f4178c42e03b99ab931434
Tim Greenwood <1031568+tgrnwd@users.noreply.github.com>
2022-02-15 12:47:57 -0600

    add project files

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # get-deployment
+
+# Get Deployment - GitHub Action
+
+This action gets details about the specified environment's deployment
+
+## Inputs
+
+## `environment`
+
+**Required** The name of the environment. Default `""`.
+
+## `params`
+
+**Required** Parameters of Deployment in the specified environment. Default `"latest active"`. Also available, `"success fail inactive"`
+
+## Outputs
+
+## `time`
+
+The time the action ran.
+
+## `ref`
+
+Ref of deployment.
+
+
+## Example usage
+
+uses: tgrnwd/get-deployment@v0.1
+with:
+  environment: 'dev'
---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const environment = core.getInput('environment');
+  console.log(`Getting ${environment}!`);
+
+
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", ${environment});
+  core.setOutput("ref", time);
+  
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
