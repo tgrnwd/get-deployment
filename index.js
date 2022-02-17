772bfe9872286686db7b9305c2ff4da4db9ca0e6
Tim Greenwood <1031568+tgrnwd@users.noreply.github.com>
2022-02-17 10:51:05 -0600

    functions

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(`Getting ${environment}!`);
 
 let page = 1
 
-async function getDeploymentsX() {
+async function getDeployments() {
   return await octokit.rest.repos.listDeployments({
     ...context.repo,
     environment: environment,
@@ -20,7 +20,7 @@ async function getDeploymentsX() {
   })
 }
 
-async function deploymentDetailsX(deployment) {
+async function deploymentStatuses(deployment) {
   return await octokit.rest.repos.listDeploymentStatuses({
     ...context.repo,
     deployment_id: deployment.id
@@ -30,25 +30,14 @@ async function deploymentDetailsX(deployment) {
   })
 }
 
-  // const deploymentDetails = (deployment) => await octokit.rest.repos.listDeploymentStatuses({
-  //   ...context.repo,
-  //   deployment_id: deployment.id
-  // }).then(data => {
-
-  //   let statuses = data.data
-  //   return statuses.map( status => {
-  //     return status.state
-  //   })
-  // })
-
 (async () => {
   try {
 
-    let deployments = await getDeploymentsX()
+    // let deployments = await getDeployments()
     // console.log(await getDeploymentsX())
 
-    let status = await deployments.map(deployment => {
-      return deploymentDetailsX(deployment).then(deploymentStatus => {
+    let status = await getDeployments().map(deployment => {
+      return deploymentStatuses(deployment).then(deploymentStatus => {
         return {
           // 'id': deployment.id,
           'statusfrommap': deploymentStatus,
@@ -73,200 +62,3 @@ async function deploymentDetailsX(deployment) {
     core.setFailed(error.message);
   }
 })()
-
-
-
-
-async function getDeployments() {
-  // const token = core.getInput('token');
-  // const octokit = new github.getOctokit(token)
-  // const context = github.context
-
-  // const environment = core.getInput('environment');
-  // console.log(`Getting ${environment}!`);
-
-  // let page = 1
-  
-  // const deployments = await octokit.rest.repos.listDeployments({
-  //   ...context.repo,
-  //   environment: environment,
-  //   page: page
-  // }).then((data) => {
-  //   let deployments = data.data
-
-  //   // console.log(deployments)
-    
-  //   deployments.map(async deployment => {
-  //     let deploymentStatus = await octokit.rest.repos.getDeployment({
-  //       ...context.repo,
-  //       deployment_id: deployment.id
-  //     }).then( (deploymentStatus) => {
-
-  //       console.log(deploymentStatus)
-
-  //       octokit.rest.repos.getDeploymentStatus({
-  //         owner,
-  //         repo,
-  //         deployment_id,
-  //         status_id,
-  //       });
-        
-  //       return deploymentStatus.state == 'success' ? {
-  //         'id': deploymentStatus.id,
-  //         'status': deploymentStatus.state,
-  //         'ref': deployment.ref
-  //       } : {}
-  //     })
-
-  //   }, [])
-    
-  // })
-
-  // deployments.then(data => console.log(data))
-
-  // const deployments = await octokit.rest.repos.listDeployments({
-  //   ...context.repo,
-  //   environment: environment,
-  //   page: page
-  // }).then( response => {
-  //   return response.data
-  // }).then(deployments => deployments.map(deployment => {
-  //   let deploymentStuff = await octokit.rest.repos.listDeploymentStatuses({
-  //     ...context.repo,
-  //     deployment_id: deployment.id
-  //   }).then(data => {
-  //     let statuses = data.data
-  //     return statuses.map( status => {
-  //       return status.state
-  //     })
-  //   })
-
-  //   return 
-  // }))
-
-  // const deploymentDetails = (deployment) => await octokit.rest.repos.listDeploymentStatuses({
-  //   ...context.repo,
-  //   deployment_id: deployment.id
-  // }).then(data => {
-
-  //   let statuses = data.data
-  //   return statuses.map( status => {
-  //     return status.state
-  //   })
-  // })
-
-  // function deploymentDetails(deployment) {
-  //   return octokit.rest.repos.listDeploymentStatuses({
-  //     ...context.repo,
-  //     deployment_id: deployment.id
-  //   }).then(data => {
-  
-  //     let statuses = data.data
-  //     return statuses.map( status => {
-  //       return status.state
-  //     })
-  //   })
-  // }
-
-  // async function mapDeploymentDetails(deployments) {
-    
-  //   return await deployments.map(deployment => {
-  //     return octokit.rest.repos.listDeploymentStatuses({
-  //       ...context.repo,
-  //       deployment_id: deployment.id
-  //     }).then(data => {
-    
-  //       let statuses = data.data
-  //       return statuses.map( status => {
-  //         return status.state
-  //       })
-  //     })
-  //   })
-  // }
-
-  // const deploymentDetails = ( deployment ) => await octokit.rest.repos.listDeploymentStatuses({
-  //   ...context.repo,
-  //   deployment_id: deployment.id
-  // })
-
-  // const deploymentstatus = ( deployment ) => await octokit.rest.repos.getDeploymentStatus({
-  //   ...context.repo,
-  //   deployment_id,
-  //   status_id,
-  // });
-
-  // console.log( await deployments )
-
-  // let d = await deployments
-
-  // let x = await deployments.then(data => mapDeploymentDetails(data))
-  // console.log( await x )
-
-  // let x = d.map( deployment => {
-
-  //   // return new Promise((resolve) => {
-
-  //   //   return {
-  //   //     "id": deployment.id,
-  //   //     "sha": deployment.sha,
-  //   //     "ref": deployment.ref,
-  //   //     "states": resolve(deploymentDetails(deployment))
-  //   //   }
-  //   // })
-
-  //   // return {
-  //   //   "id": deployment.id,
-  //   //   "sha": deployment.sha,
-  //   //   "ref": deployment.ref,
-  //   //   "states": await new Promise((resolve) => {
-  //   //     resolve(deploymentDetails(deployment))
-  //   //   })
-  //   // }
-
-  // })
-
-  // console.log( await x )
-
-  // deployments.then( deploymentDetails( data ).then( deploymentstatus( data ) ) )
-
-  // deployments.then( data.data.map())
-
-
-
-
-
-  // return deployments.map(async deployment => {
-    
-  //   let status = await octokit.rest.repos.getDeployment({
-  //     ...context.repo,
-  //     deployment_id: deployment.id
-  //   });
-
-  //   return status.state == 'success' ? {
-  //     'id': status.id,
-  //     'status': status.state,
-  //     'ref': deployment.ref
-  //   } : {}
-    
-  // }, []).filter(status => Object.keys(status).length > 0)
-
-  // return deployments
-}
-
-// try {
-
-//   let deployments = getDeployments()
-//   // console.log(deployments)
-
-//   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
-
-//   core.setOutput("ref", deployments);
-  
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   // console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
-
